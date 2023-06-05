55a97c6202fdf717af532c2f601d6ffddd2dc283
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2023-06-05 12:36:48 -0400

    groom outputs for zrok's k8s

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "kubeconfig" {
-    value = linode_lke_cluster.linode_lke.kubeconfig
+    value = digitalocean_kubernetes_cluster.zrok_cluster.kube_config[0].raw_config
     sensitive = true
 }
 
-resource "local_sensitive_file" "kubeconfig" {
-    depends_on   = [linode_lke_cluster.linode_lke]
-    filename     = "../kube-config-${terraform.workspace}"
-    content      = base64decode(linode_lke_cluster.linode_lke.kubeconfig)
-    file_permission = 0600
-}
+# resource "local_sensitive_file" "kubeconfig" {
+#     depends_on   = [linode_lke_cluster.linode_lke]
+#     filename     = "../kube-config-${terraform.workspace}"
+#     content      = base64decode(linode_lke_cluster.linode_lke.kubeconfig)
+#     file_permission = 0600
+# }
 
 output "dns_zone" {
     description = "consumed by router plan to build ingress names"
     value = var.dns_zone
 }
 
-output "ziti_namespace" {
+output "zrok_namespace" {
     description = "consumed by router plan to install release in same namespace as controller which is convenient but not necessary"
-    value = var.ziti_namespace
+    value = var.zrok_namespace
 }
 
 output "cluster_issuer_name" {
@@ -25,6 +25,6 @@ output "cluster_issuer_name" {
     value = var.cluster_issuer_name
 }
 
-output "cluster_label" {
-    value = var.label
+output "cluster_name" {
+    value = var.cluster_name
 }
